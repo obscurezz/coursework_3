8988fcf994d5d1a54048cf57cc966514aa26c5a9
Andrey Kopylov <104315220+obscurezz@users.noreply.github.com>
2022-07-03 20:52:45 +0300

    added logger for app and api

--- api/api.py
+++ api/api.py
@@ -1,19 +1,24 @@
+from logging_config import get_logger
+
 from flask import Blueprint, jsonify
 
 from utils import DataLoader
 
 POSTS = DataLoader('data/data.json')
+logger = get_logger(__name__, 'logs/api.log')
 
 api_blueprint = Blueprint('api_blueprint', __name__)
 
 
 @api_blueprint.route('/api/posts', methods=['GET'])
 def get_json_posts():
+    logger.info('Request to /api/posts')
     posts = POSTS.get_posts_all()
     return jsonify(posts)
 
 
 @api_blueprint.route('/api/posts/<int:post_id>', methods=['GET'])
 def get_json_post_by_id(post_id: int):
+    logger.info(f'Request to /api/posts/{post_id}')
     post = POSTS.get_post_by_pk(post_id)
     return jsonify(post)
--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from os import path, mkdir
 from flask import Flask, render_template
 
 from config import LOGS_FOLDER
+from logging_config import get_logger
 from main.main import main_blueprint, posts_blueprint
 from search.search import search_blueprint
 from user_feed.user_feed import user_feed_blueprint
@@ -10,6 +11,8 @@ from api.api import api_blueprint
 
 if not path.exists(LOGS_FOLDER):
     mkdir(LOGS_FOLDER)
+
+logger = get_logger(__name__, 'logs/app.log')
 
 app = Flask(__name__, template_folder='templates')
 
@@ -22,11 +25,13 @@ app.register_blueprint(api_blueprint)
 
 @app.errorhandler(404)
 def not_found_error(error):
+    logger.error('Something went wrong with errcode 404')
     return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(error):
+    logger.error('Something went wrong with errcode 500')
     return render_template('500.html'), 500
 
 
